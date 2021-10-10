0f9f1cf12d4bf4a8a0b42c362eaa22f88307dea0
Adit <adityahimaone@gmail.com>
2021-10-10 16:04:15 +0700

    WD Req/Res Users

---
+++ app/presenter/users/request/users.go
@@ -0,0 +1,26 @@
+package request
+
+import (
+	"todolist/bussiness/users"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+	DOB      string `json:"dob"`
+}
+
+type UserLogin struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func ToDomain(request User) users.Domain {
+	return users.Domain{
+		Username: request.Username,
+		Password: request.Password,
+		Name:     request.Name,
+		DOB:      request.DOB,
+	}
+}
